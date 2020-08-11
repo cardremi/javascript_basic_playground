3c1abc9cf3b9b96d25e4edfed33061353915e6ff
Prieyudha Akadita S <akaditasustono@gmail.com>
2020-08-11 21:36:42 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//selector, just like findViewById
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.getElementById('todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//event listener, just like listener in Android
+todoButton.addEventListener('click', addTodo);
+
+//func
+function addTodo(){
+    console.log('ashiap ashiap');
+
+}
